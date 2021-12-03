622658d71e54f92f12a0a956f89894de0d618deb
Weixi779 <49645134+Weixi779@users.noreply.github.com>
2021-12-03 18:55:45 +0800

    Add files via upload

---
+++ MQTT-service/MQTT-service/MQTT_serviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MQTT_serviceApp.swift
+//  MQTT-service
+//
+//  Created by yunzhanghu1186 on 2021/12/3.
+//
+
+import SwiftUI
+
+@main
+struct MQTT_serviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
